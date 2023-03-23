200790ad40aa7dd6e77bf2d80c32bcafa581a124
hbqhbq <117273547+hbqhbq@users.noreply.github.com>
2023-03-23 16:53:25 +0800

    Update example.py

--- example.py
+++ example.py
@@ -15,6 +15,12 @@ from sklearn.preprocessing import MinMaxScaler
 import time, random, json
 from sklearn.model_selection import train_test_split
 
+from imblearn.pipeline import Pipeline
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE, ADASYN, RandomOverSampler
+from imblearn.under_sampling import ClusterCentroids,RandomUnderSampler,TomekLinks
+
 
 # normalize trian and test data
 def preprocess(df_train, df_test):
@@ -38,6 +44,46 @@ def preprocess(df_train, df_test):
     return df_train, df_test
 
 
+def sampling_data_prep(method, X_train, y_train):
+    print("\n本次采样方法：{}".format(method))
+    if (method == "tomeklink"):
+        undersample = TomekLinks()
+        x, y = undersample.fit_resample(X_train, y_train)
+
+    elif (method == "smote"):
+        oversample = SMOTE()
+        x, y = oversample.fit_resample(X_train, y_train)
+
+    elif (method == "adasyn"):
+        oversample = ADASYN()
+        x, y = oversample.fit_resample(X_train, y_train)
+
+    elif (method == 'randomover'):
+        oversample = RandomOverSampler(random_state=0)
+        x, y = oversample.fit_resample(X_train, y_train)
+
+    elif(method == 'nonsampling'):
+        x, y = X_train, y_train
+
+    elif (method == 'combine_over_and_under_sampling'):
+        over = SMOTE()
+        under = TomekLinks()
+        # define pipeline
+        pipeline = Pipeline(steps=[('o', over), ('u', under)])
+        x, y = pipeline.fit_resample(X_train, y_train)
+
+    count_fraud = 0
+    count_nonfraud = 0
+    for flag in y:
+        if flag == 0:
+            count_fraud += 1
+        else:
+            count_nonfraud += 1
+    print("number of fraud: {}, number of non fraud: {}".format(count_fraud, count_nonfraud))
+
+    return x, y
+
+
 def main():
     """# Import data"""
 
@@ -57,16 +103,30 @@ def main():
     Y = df["Class"].values
 
     # train test split
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    X_train, X_test, Y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
     total_fit = 0
     total_predict = 0
 
     entities = 1
+    
+    #采样方式名称
+    sample_name = ['tomeklink', 'smote', 'adasyn', 'randomover', 'combine_over_and_under_sampling', 'nonsampling']
+    i = 0
 
-    for i in range(entities):
-
-        x_train, x_test = preprocess(np.asarray(X_train), np.asarray(X_test))
+    X_train, x_test = preprocess(np.asarray(X_train), np.asarray(X_test))
+    
+    #每种采样方式的分数存在这些列表里
+    sampling_train_score = []
+    sampling_test_score = []
+    sampling_m = []
+    sampling_threshold = []
+    
+    
+    #最终训练集x_train, y_train,测试集x_test, y_test 都是小写
+    for name in sample_name:
+        
+        x_train, y_train = sampling_data_prep(name, X_train, Y_train)
 
         # init model
         model = USAD(x_dims=config.x_dims[config.dataset], max_epochs=config.max_epochs[config.dataset],
@@ -100,6 +160,7 @@ def main():
 
         # get train score
         train_score = model.predict(x_train)
+        sampling_train_score.append(train_score)
 
         if not os.path.exists(config.result_dir + f'/{i}'):
             os.mkdir(config.result_dir + f'/{i}')
@@ -111,6 +172,7 @@ def main():
         # get test score
         start_predict = time.time()
         test_score = model.predict(x_test)
+        sampling_test_score.append(test_score)
         end_predict = time.time()
         total_predict += (end_predict - start_predict)
         if config.test_score_filename:
@@ -124,13 +186,25 @@ def main():
             file.write(str(threshold))
         with open(os.path.join(config.result_dir, f'{i}/score'), 'w') as file:
             file.write(str(m))
+            
+        sampling_m.append(m)
+        sampling_threshold.append(threshold)
+    
+    i+=1
+    print(f'\n总训练耗时: {total_fit}, 总预测耗时: {total_predict}')
+   
+    j = 0
+    print("\n")
+    for name in sample_name:
 
-    print(f'总训练耗时: {total_fit}, 总预测耗时: {total_predict}')
+        print("USAD with {} sample method:".format(name))
+        print("train score: {}".format(sampling_train_score[j]))
+        print("test score: {}".format(sampling_test_score[j]))
+        print("f1: {}, precision: {}, recall: {}, TP: {}, TN: {}, FP: {}, FN: {}, latency: {}".format(sampling_m[j][0],sampling_m[j][1],sampling_m[j][2],sampling_m[j][3],sampling_m[j][4],sampling_m[j][5],sampling_m[j][6],sampling_m[j][7]))
+        print("best threshold: {}\n".format(sampling_threshold[j]))
+        j += 1
 
 
 if __name__ == '__main__':
-    # get config
-    config = ConfigHandler().config
-    print('Configuration:')
     print(config)
     main()
